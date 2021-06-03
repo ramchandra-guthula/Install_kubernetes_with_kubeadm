6495ee44aa8a532417ea6c236d24e07d0558fa68
Ramchandra Guthula <64400940+ramchandra-guthula@users.noreply.github.com>
2021-06-03 14:18:15 +0530

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -2,7 +2,7 @@
 
 echo -e "\nChecking docker and ansible installations \n"
 
-##############function to install Ansible######################
+############## function to install Ansible ######################
 ansible_install(){
 ansible --version >> /dev/null
 if [ $? != 0 ];
@@ -13,7 +13,7 @@ echo -e "============Good Ansible is already installed===================\n"
 fi
 }
 
-##################Installing Boto3###########################
+################## Installing Boto3 ###########################
 boto3_install(){
 pip show boto3 >> /dev/null
 if [ $? != 0 ];
@@ -27,7 +27,7 @@ fi
 }
 
 
-#####################Function to install Docker########################
+##################### Function to install Docker ########################
 docker_install(){
 docker --version >> /dev/null
 if [ $? != 0 ];
@@ -36,13 +36,22 @@ yum install docker -y
 service docker enable
 service docker start
 else
-echo -e "====================Docker already Installed====================\n"
+echo -e "==================== Docker already Installed ====================\n"
 fi
 }
 ansible_install
 docker_install
 boto3_install
 
+echo -e "========================== Finding Devops.pem keypair ==================\n"
+if [ -f $PWD/devops.pem ]
+  then
+    echo -e  "========================== Devops.pem Keypair exists====================\n"
+    chmod 400 $PWD/devops.pem
+  else
+    echo -e "========================== Devops Keypar doesn't exist ==================\n"
+    break
+fi
 
 echo "+------------------------------------------+"
 printf "| Creating Kube cluster with Kubeadm in AWS |\n" "`date`"
@@ -53,15 +62,15 @@ read anykey
 
 ############ Getting  vars from the Vars ##############
 
-echo "Enter your VPC ID =>"
+echo "Enter your VPC ID ==>"
 read vpc
-echo "Enter your Region =>"
+echo "Enter your Region ==>"
 read region_name
-echo "Name of your private key without .pem => "
+echo "Name of your private key without .pem ==> "
 read private_key_name
-echo " Enter your subnet ID =>"
+echo " Enter your subnet ID ==>"
 read subnet
-echo "Enter your Image ID =>"
+echo "Enter your Image ID ==>"
 read image
 
 echo -e "Creating cluster with following details \n-\e[32m$vpc \n-\e[32m$region_name \n-\e[32m$private_key_name \n-\e[32m$subnet \n-\e[32m$image \n"
